85c917b29e97f130e5105a1813213feb2924a2a2
Roony11-1 <eltheasasin@gmail.com>
2025-08-16 18:22:37 -0400

    button adjustement

--- src/Body/Seccion.jsx
+++ src/Body/Seccion.jsx
@@ -5,7 +5,7 @@ export function Seccion({ titulo, items }) {
     return (
         <>
             <section className="main-seccion">
-                <h2>{titulo}</h2>
+                <h2 className="titulo-seccion">{titulo}</h2>
                 <article className="body-seccion">
                     {items.map((item, index) => (
                         <Contenedor
